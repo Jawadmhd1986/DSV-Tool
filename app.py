0dd8a485452c05c7fc11f35015b74b135205ec79
Jawadmhd1986 <mohamad.jawad@dsv.com>
2025-08-07 13:03:17 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1185,9 +1185,10 @@ def chat():
     r"dsv location", r"dsv abu dhabi location", r"where is dsv", r"dsv address", r"main office location", 
     r"where is dsv abu dhabi", r"location of dsv", r"where.*dsv.*located", r"head office address"]):
         return jsonify({"reply":
-        "📍 **DSV Abu Dhabi Location:**\n"
-        "M-19, Mussafah Industrial Area, Abu Dhabi, UAE\n"
-        "📞 +971 2 555 2900"})
+        "📍 <strong>DSV Abu Dhabi Location:</strong><br>"
+        "M-19, Mussafah Industrial Area, Abu Dhabi, UAE<br>"
+        "📞 +971 2 555 2900<br><br>"
+        "🗺️ <a href='https://goo.gl/maps/tnFcmydbvdJ9gGLy8' target='_blank'>Open on Google Maps</a>"})
 
     # --- Friendly Chat ---
     if match([r"\bhello\b|\bhi\b|\bhey\b|good morning|good evening"]):
